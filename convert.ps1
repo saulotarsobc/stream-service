c3d9f933533819bbf92308be96a93b11b9bf5fc2
saulotarsobc <saulotarsobc@gmail.com>
2025-01-19 15:32:52 -0300

    :fire:

--- convert.ps1
+++ convert.ps1
@@ -1,28 +1,24 @@
-# Defina o caminho do arquivo de entrada e saída
-$inputFile = "aulas/original/js/0001.mp4"
-$outputDir = "aulas/js/0001/"
+$inputFile = "./videos/big_buck_bunny/intro.avi"
+$outputDir = "./temp/big_buck_bunny/intro"
 
-# Crie o diretório de saída, se não existir
 if (!(Test-Path -Path $outputDir)) {
     New-Item -ItemType Directory -Path $outputDir
 }
 
-# Comando FFmpeg para gerar HLS em múltiplas resoluções
 ffmpeg -i $inputFile `
--vf "scale=426:240" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 96k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/240p_%03d.ts" "$outputDir/240p.m3u8" `
--vf "scale=640:360" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 128k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/360p_%03d.ts" "$outputDir/360p.m3u8" `
--vf "scale=854:480" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 160k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/480p_%03d.ts" "$outputDir/480p.m3u8" `
--vf "scale=1920:1080" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 192k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/1080p_%03d.ts" "$outputDir/1080p.m3u8"
+-vf "scale=w=426:h=360:force_original_aspect_ratio=decrease,pad=426:360:(ow-iw)/2:(oh-ih)/2" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 96k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/360p_%03d.ts" "$outputDir/360p.m3u8" `
+-vf "scale=w=640:h=480:force_original_aspect_ratio=decrease" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 128k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/480p_%03d.ts" "$outputDir/480p.m3u8" `
+-vf "scale=w=854:h=720:force_original_aspect_ratio=decrease" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 160k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/720p_%03d.ts" "$outputDir/720p.m3u8" `
+-vf "scale=w=1920:h=1080:force_original_aspect_ratio=decrease" -c:v libx264 -preset veryfast -crf 23 -c:a aac -b:a 192k -hls_time 6 -hls_playlist_type vod -hls_segment_filename "$outputDir/1080p_%03d.ts" "$outputDir/1080p.m3u8"
 
-# Criar playlist mestre
 $masterPlaylist = @"
 #EXTM3U
-#EXT-X-STREAM-INF:BANDWIDTH=400000,RESOLUTION=426x240
-240p.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360
+#EXT-X-STREAM-INF:BANDWIDTH=400000,RESOLUTION=426x360
 360p.m3u8
-#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=854x480
+#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x480
 480p.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=854x720
+720p.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=4000000,RESOLUTION=1920x1080
 1080p.m3u8
 "@
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
+import cors from "cors";
 import express, { Request, Response } from "express";
 import { join } from "node:path";
-import { existsSync } from "node:fs";
-import cors from "cors";
 
 const app = express();
 app.use(cors());
 
 // Rota para servir os arquivos HLS de forma estática
-app.use("/aulas/js", express.static(join(__dirname, "../aulas/js")));
+app.use(
+  "/big_buck_bunny/",
+  express.static(join(__dirname, "../temp/big_buck_bunny/intro"))
+);
 
 // Página principal
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
   res.sendFile(join(__dirname, "../static/index.html"));
 });
 
 // Rota para verificar a playlist
-app.get("/aulas/js/0001", (req: any, res: any) => {
-  const playlistPath = join(__dirname, "../aulas/js/0001/master.m3u8");
-
-  console.log(`Playlist path: ${playlistPath}`);
-
-  if (!existsSync(playlistPath)) {
-    return res.status(404).send("A playlist HLS não foi encontrada.");
-  }
-
+app.get("/big_buck_bunny/intro", (_req: Request, res: Response) => {
+  const playlistPath = join(
+    __dirname,
+    "../temp/big_buck_bunny/intro/master.m3u8"
+  );
   res.sendFile(playlistPath);
 });
 
